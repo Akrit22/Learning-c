5fa51f7a6c4e59cc9738102a320564644102863f
Akrit Pathania <akritpathania22@gmail.com>
2024-07-22 11:37:06 -0700

    2D_arrary.c

--- 2D_arrary.c
+++ 2D_arrary.c
@@ -2,9 +2,9 @@
 int main()
 {
     int a[10][10],b[10][10],n,m;
-    printf("enter the number of elements in row:");
+    printf("Enter the number of elements in row:");
     scanf("%d",&n);
-    printf("enter the number of elements in column:");
+    printf("Enter the number of elements in column:");
     scanf("%d",&m);
     int sum[10][10],subt[10][10],multi[10][10];
     float div[10][10];
@@ -12,14 +12,14 @@ int main()
     for(i=0;i<n;i++)
     {
         for(j=0;j<m;j++){
-        printf("enter the elements of a[%d][%d]:",i,j);
+        printf("Enter the elements of a[%d][%d]:",i,j);
         scanf("%d",&a[i][j]);
         }
     }
     for(i=0;i<n;i++)
     {
         for(j=0;j<m;j++){
-        printf("enter the elements of b[%d][%d]:",i,j);
+        printf("Enter the elements of b[%d][%d]:",i,j);
         scanf("%d",&b[i][j]);
         }
     }
@@ -32,14 +32,14 @@ int main()
             div[i][j]=a[i][j]/b[i][j];
         }
     }    
-    printf("\n addition\n");
+    printf("\n Addition\n");
      for(i=0;i<n;i++){
         printf("\n");
         for(j=0;j<m;j++){
         printf("%d\t",sum[i][j]);
         }
     }   
-    printf("\n subtract\n");
+    printf("\n Subtract\n");
 
      for(i=0;i<n;i++){
         printf("\n");
@@ -47,14 +47,14 @@ int main()
         printf("%d\t",subt[i][j]);
         }
     }   
-    printf("\n muliply\n");
+    printf("\n Muliply\n");
      for(i=0;i<n;i++){
         printf("\n");
         for(j=0;j<m;j++){
         printf("%d\t",multi[i][j]);
         }
     }   
-    printf("\n divide\n");
+    printf("\n Divide\n");
      for(i=0;i<n;i++){
         printf("\n");
         for(j=0;j<m;j++){
